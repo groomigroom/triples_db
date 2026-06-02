0f7245d74521f59256fad26e5cb303f3f47f6dc0
groomigroom <googjohn0143@gmail.com>
2026-06-02 16:13:00 +0900

    Update LOVE&POP_pt1.sql

--- sql_db/LOVE&POP_pt1.sql
+++ sql_db/LOVE&POP_pt1.sql
@@ -2,17 +2,17 @@ create table love_and_pop_pt1
 (
   title varchar(50),
   track_num integer,
-  song_len integer,
+  song_len time,
   melon_like integer,
   youtube_like integer
 );
 
 insert into love_and_pop_pt1
 values
-("Sad Girls Schemin", 1, 곡길이, 526, 229),
-("Peer", 2, 곡길이, 418, 109),
-("Baby Flower", 3, 곡길이, 1780, 31000),
-("Type of Girl", 4, 곡길이, 442, 159),
-("Sleek", 5, 곡길이, 458, 151),
-("I Like That", 6, 곡길이, 428, 142),
-("Me Myself Mode", 7, 곡길이, 463, 147);
+("Sad Girls Schemin", 1, "00:02:42", 526, 229),
+("Peer", 2, "00:01:01", 418, 109),
+("Baby Flower", 3, "00:03:08", 1780, 31000),
+("Type of Girl", 4, "00:03:09", 442, 159),
+("Sleek", 5, "00:02:31", 458, 151),
+("I Like That", 6, "00:02:16", 428, 142),
+("Me Myself Mode", 7, "00:02:19", 463, 147);
