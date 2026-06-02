73c602d6a865348bcd3c2bec4079a2b6b0d1fd74
groomigroom <googjohn0143@gmail.com>
2026-06-02 14:46:50 +0900

    Update LOVE&POP_pt1.sql

--- sql_db/LOVE&POP_pt1.sql
+++ sql_db/LOVE&POP_pt1.sql
@@ -9,4 +9,10 @@ create table love_and_pop_pt1
 
 insert into love_and_pop_pt1
 values
-("Sad Girls Schemin", 1, ?, 526, ),
+("Sad Girls Schemin", 1, 곡길이, 526, 유튜브좋아요수),
+("Peer", 2, 곡길이, 418, 유튜브좋아요수),
+("Baby Flower", 3, 곡길이, 1780, 유튜브좋아요수),
+("Type of Girl", 4, 곡길이, 442, 유튜브좋아요수),
+("Sleek", 5, 곡길이, 458, 유튜브좋아요수),
+("I Like That", 6, 곡길이, 428, 유튜브좋아요수),
+("Me Myself Mode", 7, 곡길이, 463, 유튜브좋아요수),
