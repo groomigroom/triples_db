0c5acfcdd069cbcb2399bfbddf2cfcf7313bacea
groomigroom <googjohn0143@gmail.com>
2026-06-03 17:35:55 +0900

    Create Tokimetique.sql

---
+++ sql_db/Tokimetique.sql
@@ -0,0 +1,14 @@
+create table Tokimetique
+(
+title varchar(50),
+track_num integer,
+song_len time,
+melon_like integer,
+youtube_like integer
+);
+
+insert into Tokimetique
+values
+("Tokimetique", 1, "00:02:45", 219, 2100),
+("Tokimetique -Shin Sakiura Remix-", 2, "00:03:04", 106, 861),
+("Tokimetique TV Edit", 3, "00:01:04", 89, 2100);
