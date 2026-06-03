2d1c89bfaa8074bdbc5c2a65337f20a3ff4ddc38
groomigroom <googjohn0143@gmail.com>
2026-06-03 20:57:03 +0900

    Update edit_sub_query.sql

--- sql_db/query/edit_sub_query.sql
+++ sql_db/query/edit_sub_query.sql
@@ -138,7 +138,37 @@ from triples_start_unit b
 where a.s_id = b.s_id) start_unit
 from triples_members a;
 
-
+/*
+출력값
++------+-----------+----------------------+
+| s_id | name_bar  | start_unit           |
++------+-----------+----------------------+
+|    1 | 윤서연    | +(KR)ystal Eyes      |
+|    2 | 정혜린    | Acid Angel from Asia |
+|    3 | 이지우    | +(KR)ystal Eyes      |
+|    4 | 김채연    | +(KR)ystal Eyes      |
+|    5 | 김유연    | Acid Angel from Asia |
+|    6 | 김수민    | +(KR)ystal Eyes      |
+|    7 | 김나경    | Acid Angel from Asia |
+|    8 | 공유빈    | Acid Angel from Asia |
+|    9 | 카에데    | LOVElution           |
+|   10 | 서다현    | LOVElution           |
+|   11 | 코토네    | EVOLution            |
+|   12 | 곽연지    | EVOLution            |
+|   13 | 니엔      | LOVElution           |
+|   14 | 박소현    | LOVElution           |
+|   15 | 신위      | LOVElution           |
+|   16 | 마유      | EVOLution            |
+|   17 | 린        | NXT                  |
+|   18 | 주빈      | NXT                  |
+|   19 | 정하연    | NXT                  |
+|   20 | 박시온    | NXT                  |
+|   21 | 김채원    | Glow                 |
+|   22 | 설린      | Glow                 |
+|   23 | 서아      | Glow                 |
+|   24 | 지연      | Glow                 |
++------+-----------+----------------------+
+*/
 
 select a.s_id, a.name_bar, 
 (select concat(b.name_bar, " / ", b.unit)
