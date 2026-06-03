827f2eadfd561e1a02c45c7f44b015db7c7583eb
groomigroom <googjohn0143@gmail.com>
2026-06-03 17:50:03 +0900

    Create msnz_Beyond_Beauty table and insert records

---
+++ sql_db/edit_msnz_<Beyond Beauty>.sql
@@ -0,0 +1,17 @@
+create table msnz_Beyond_Beauty
+(
+title varchar(50),
+track_num integer,
+song_len time,
+melon_like integer,
+youtube_like integer
+);
+
+insert into msnz_Beyond_Beauty
+values
+("Magic Shine New Zone", 1, "00:02:45", 219, ),
+("Fly Up", 2, "00:03:04", 106, ),
+("Cameo Love", 3, "00:01:04", 89, ),
+("Bubble Gum Girl", 4, "00:01:04", 89, ),
+("Q&A", 5, "00:01:04", 89, ),
+("Christmas Alone", 6, "00:01:04", 89, );
