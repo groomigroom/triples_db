912051e754287006b5a2f9bce787b2625e467eba
groomigroom <googjohn0143@gmail.com>
2026-06-04 13:57:02 +0900

    Update edit_4study4work4inst_Vol.3.sql

--- sql_db/edit_4study4work4inst_Vol.3.sql
+++ sql_db/edit_4study4work4inst_Vol.3.sql
@@ -9,17 +9,17 @@ youtube_like integer
 
 insert into 4study4work4inst_Vol.3
 values
-("깨어 (Are You Alive) (Inst.)", 1, "00:03:04", 156, ), 
-("추리소설 (Detective Soseol) (Inst.)", 2, "00:03:20", 120, ),
-("어제 우리 불꽃놀이 (Firework Diary) (Inst.)", 3, "00:03:08", 108, ), 
-("Love Child (Inst.)", 4, "00:03:01", 97, ), 
-("Persona (Inst.)", 5, "00:03:02", 93, ), 
-("Too Hot (Inst.)", 6, "00:03:21", 101, ), 
-("Diablo (Inst.)", 7, "00:03:20", 104, ), 
-("Friend Zone (Inst.)", 8, "00:03:17", 98, ), 
-("Love2Love (Inst.)", 9, "00:03:10", 96, ), 
-("Fly Up (Inst.)", 10, "00:02:40", 98, ), 
-("Cameo Love (Inst.)", 11, "00:02:35", 116, ), 
-("Bubble Gum Girl (Inst.)", 12, "00:02:48", 91, ), m
-("Q&A (Inst.)", 13, "00:01:04", 89, ),
-("Christmas Alone (Inst.)", 14, "00:01:04", 89, );
+("깨어 (Are You Alive) (Inst.)", 1, "00:03:04", 156, 2300), 
+("추리소설 (Detective Soseol) (Inst.)", 2, "00:03:20", 120, 52),
+("어제 우리 불꽃놀이 (Firework Diary) (Inst.)", 3, "00:03:08", 108, 44), 
+("Love Child (Inst.)", 4, "00:03:01", 97, 34), 
+("Persona (Inst.)", 5, "00:03:02", 93, 33), 
+("Too Hot (Inst.)", 6, "00:03:21", 101, 38), 
+("Diablo (Inst.)", 7, "00:03:20", 104, 50), 
+("Friend Zone (Inst.)", 8, "00:03:17", 98, 36), 
+("Love2Love (Inst.)", 9, "00:03:10", 96, 36), 
+("Fly Up (Inst.)", 10, "00:02:40", 98, 40), 
+("Cameo Love (Inst.)", 11, "00:02:35", 116, 67), 
+("Bubble Gum Girl (Inst.)", 12, "00:02:48", 91, 31), m
+("Q&A (Inst.)", 13, "00:03:26", 95, ),
+("Christmas Alone (Inst.)", 14, "00:02:51", 98, );
