0448812b8fbf6affb0bdc91c4bfa89725cb08ad5
groomigroom <googjohn0143@gmail.com>
2026-06-04 15:03:24 +0900

    Update edit_msnz_<Beyond Beauty>.sql

--- sql_db/edit_msnz_<Beyond Beauty>.sql
+++ sql_db/edit_msnz_<Beyond Beauty>.sql
@@ -9,9 +9,9 @@ youtube_like integer
 
 insert into msnz_Beyond_Beauty
 values
-("Magic Shine New Zone", 1, "00:02:45", 219, ),
-("Fly Up", 2, "00:03:04", 106, ),
-("Cameo Love", 3, "00:01:04", 89, ),
-("Bubble Gum Girl", 4, "00:01:04", 89, ),
-("Q&A", 5, "00:01:04", 89, ),
+("Magic Shine New Zone", 1, "00:00:51", 625, ),
+("Fly Up", 2, "00:02:40", 1426, ),
+("Cameo Love", 3, "00:02:35", 2138, ),
+("Bubble Gum Girl", 4, "00:0:48", 1097, ),
+("Q&A", 5, "00:03:26", 1104, ), m
 ("Christmas Alone", 6, "00:01:04", 89, );
