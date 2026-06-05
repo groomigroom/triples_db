994798079c4ac85ad9eceb8b13fb4ff767fa6ebc
groomigroom <googjohn0143@gmail.com>
2026-06-05 16:33:13 +0900

    Update tripleS_hatch!_SecretHimitsuBimil.sql

--- sql_db/tripleS_hatch!_SecretHimitsuBimil.sql
+++ sql_db/tripleS_hatch!_SecretHimitsuBimil.sql
@@ -1,8 +1,16 @@
 create table tripleS_hatch!_SecretHimitsuBimil
 (
-title varchar(50),
-track_num integer,
-song_len time,
-melon_like integer,
-youtube_like integer
+  title varchar(50),
+  track_num integer,
+  song_len time,
+  melon_like integer,
+  youtube_like integer
 );
+
+insert into tripleS_hatch!_SecretHimitsuBimil
+values
+title varchar(50),
+  track_num integer,
+  song_len time,
+  melon_like integer,
+  youtube_like integer
