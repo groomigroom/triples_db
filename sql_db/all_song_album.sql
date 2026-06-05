1b24939179ed550dceee67f44b6318b8766ea4bf
groomigroom <googjohn0143@gmail.com>
2026-06-05 22:03:31 +0900

    Update all_song_album.sql

--- sql_db/all_song_album.sql
+++ sql_db/all_song_album.sql
@@ -7,4 +7,13 @@ create table all_song_album
 
 insert into all_song_album
 values
-(),
+(1, "Sad Girls Schemin", "love_and_pop_pt1"),
+(1, "Sad Girls Schemin", "love_and_pop_pt1"),
+
+
+("Peer", 2, "00:01:01", 418, 109),
+("Baby Flower", 3, "00:03:08", 1780, 31000),
+("Type of Girl", 4, "00:03:09", 442, 159),
+("Sleek", 5, "00:02:31", 458, 151),
+("I Like That", 6, "00:02:16", 428, 142),
+("Me Myself Mode", 7, "00:02:19", 463, 147);
