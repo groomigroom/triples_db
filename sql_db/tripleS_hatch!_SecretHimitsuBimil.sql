ca12d2ea622a0f968ff2d1d093553ce08bd97c04
groomigroom <googjohn0143@gmail.com>
2026-06-05 16:50:09 +0900

    Update tripleS_hatch!_SecretHimitsuBimil.sql

--- sql_db/tripleS_hatch!_SecretHimitsuBimil.sql
+++ sql_db/tripleS_hatch!_SecretHimitsuBimil.sql
@@ -12,4 +12,9 @@ values
 ("Password", 1, "00:00:00", 400, youtube_like integer),
 ("Headphones", 2, "00:00:00", 255, youtube_like integer),
 ("Headphones", 2, "00:00:00", 255, youtube_like integer),
+  Tokimetique 261
+  TOKYO 274
+  Oshare 346
+  Untitled 190
+  ### (hatch! Version) 201
   ;
