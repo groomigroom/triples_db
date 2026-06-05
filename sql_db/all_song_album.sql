caee014189743bff29bc50ebaeaca403224bf8de
groomigroom <googjohn0143@gmail.com>
2026-06-06 08:09:46 +0900

    Update all_song_album.sql

--- sql_db/all_song_album.sql
+++ sql_db/all_song_album.sql
@@ -59,3 +59,5 @@ values
 (10, "Love2Love", "<ASSEMBLE25>"),
 (1, "Girls Never Die (Japanese Version)", "Girls Never Die (Japanese Version)"),
 (1, "Rising (Japanese Version)", "Rising (Japanese Version)"),
+(1, "Dreaming (by 유연, 유빈, 다현, 시온)", "Dreaming (골 때리는 그녀들 X tripleS (트리플에스))"),
+(2, "Dreaming (by 유연, 유빈, 다현, 시온)", "Dreaming (by 유연, 유빈, 다현, 시온) (Inst.)"),
