3888babefe46a122cbcb1682ae1f840613db5b2c
groomigroom <googjohn0143@gmail.com>
2026-06-05 22:14:06 +0900

    Update all_song_album.sql

--- sql_db/all_song_album.sql
+++ sql_db/all_song_album.sql
@@ -17,4 +17,21 @@ values
 (1, "Tokimetique", "Tokimetique"),
 (2, "Tokimetique -Shin Sakiura Remix-", "Tokimetique"),
 (3, "Tokimetique TV Edit", "Tokimetique"),
+(1, "깨어 (Are You Alive) (Inst.)", "4study4work4inst_Vol.3"),
+(2, "추리소설 (Detective Soseol) (Inst.)", "4study4work4inst_Vol.3"),
+(3, "어제 우리 불꽃놀이 (Firework Diary) (Inst.)", "4study4work4inst_Vol.3"),
+(4, "Love Child (Inst.)", "4study4work4inst_Vol.3"),
+(5, "Persona (Inst.)", "4study4work4inst_Vol.3"),
+(6, "Too Hot (Inst.)", "4study4work4inst_Vol.3"),
+(7, "Diablo (Inst.)", "4study4work4inst_Vol.3"),
+(8, "Friend Zone (Inst.)", "4study4work4inst_Vol.3"),
+(9, "Love2Love (Inst.)", "4study4work4inst_Vol.3"),
+(10, "Fly Up (Inst.)", "4study4work4inst_Vol.3"),
+(11, "Fly Up (Inst.)", "4study4work4inst_Vol.3"),
 
+
+ 
+("Cameo Love (Inst.)", 11, "00:02:35", 116, 67), 
+("Bubble Gum Girl (Inst.)", 12, "00:02:48", 91, 31),
+("Q&A (Inst.)", 13, "00:03:26", 95, 37),
+("Christmas Alone (Inst.)", 14, "00:02:51", 98, 29);
