9abad4ea116a1be88b760521c0ecb8e5a30c0c18
groomigroom <googjohn0143@gmail.com>
2026-06-05 22:07:14 +0900

    Create all_song_selling.sql

---
+++ sql_db/all_song_selling.sql
@@ -0,0 +1,16 @@
+create table all_song_selling
+(
+  title varchar(50),
+  album varchar(50),
+  판매(억단위) int
+);
+
+insert into all_song_selling
+values
+("Sad Girls Schemin", "love_and_pop_pt1", 11),
+("Peer", "love_and_pop_pt1", 22),
+("Baby Flower", "love_and_pop_pt1", 95),
+("Type of Girl", "love_and_pop_pt1", 12),
+("Sleek", "love_and_pop_pt1", 66),
+("I Like That", "love_and_pop_pt1", 44),
+("Me Myself Mode", "love_and_pop_pt1", 77),
