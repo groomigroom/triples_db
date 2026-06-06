8c7e4d0aeb3cdecee5979d4a9cc75c3c079b9aed
groomigroom <googjohn0143@gmail.com>
2026-06-06 18:06:01 +0900

    Update sub_query_three.sql

--- sql_db/query/sub_query_three.sql
+++ sql_db/query/sub_query_three.sql
@@ -640,3 +640,213 @@ where title in ("Dreaming (by 유연, 유빈, 다현, 시온) (Inst.)", "Baby Fl
 | Dreaming (by 유연, 유빈, 다현, 시온)                |
 +-----------------------------------------------------+
 */
+
+create table all_song_album
+(
+  so_id int not null auto_increment primary key,
+  track int,
+  title varchar(50),
+  album varchar(50)
+);
+
+insert into all_song_album (track, title, album) values
+(1, "Baby Flower Japanese Version", "Baby Flower Japanese Version"),
+(1, "Sad Girls Schemin", "love_and_pop_pt1"),
+(2, "Peer", "love_and_pop_pt1"),
+(3, "Baby Flower", "love_and_pop_pt1"),
+(4, "Type of Girl", "love_and_pop_pt1"),
+(5, "Sleek", "love_and_pop_pt1"),
+(6, "I Like That", "love_and_pop_pt1"),
+(7, "Me Myself Mode", "love_and_pop_pt1"),
+(1, "Tokimetique", "Tokimetique"),
+(2, "Tokimetique -Shin Sakiura Remix-", "Tokimetique"),
+(3, "Tokimetique TV Edit", "Tokimetique"),
+(1, "깨어 (Are You Alive) (Inst.)", "4study4work4inst_Vol.3"),
+(2, "추리소설 (Detective Soseol) (Inst.)", "4study4work4inst_Vol.3"),
+(3, "어제 우리 불꽃놀이 (Firework Diary) (Inst.)", "4study4work4inst_Vol.3"),
+(4, "Love Child (Inst.)", "4study4work4inst_Vol.3"),
+(5, "Persona (Inst.)", "4study4work4inst_Vol.3"),
+(6, "Too Hot (Inst.)", "4study4work4inst_Vol.3"),
+(7, "Diablo (Inst.)", "4study4work4inst_Vol.3"),
+(8, "Friend Zone (Inst.)", "4study4work4inst_Vol.3"),
+(9, "Love2Love (Inst.)", "4study4work4inst_Vol.3"),
+(10, "Fly Up (Inst.)", "4study4work4inst_Vol.3"),
+(11, "Cameo Love (Inst.)", "4study4work4inst_Vol.3"),
+(12, "Bubble Gum Girl (Inst.)", "4study4work4inst_Vol.3"),
+(13, "Q&A (Inst.)", "4study4work4inst_Vol.3"),
+(14, "Christmas Alone (Inst.)", "4study4work4inst_Vol.3"),
+(1, "Magic Shine New Zone", "msnz_Beyond_Beauty"),
+(2, "Fly Up", "msnz_Beyond_Beauty"),
+(3, "Cameo Love", "msnz_Beyond_Beauty"),
+(4, "Bubble Gum Girl", "msnz_Beyond_Beauty"),
+(5, "Q&A", "msnz_Beyond_Beauty"),
+(6, "Christmas Alone", "msnz_Beyond_Beauty"),
+(1, "Password", "tripleS_hatch!_SecretHimitsuBimil"),
+(2, "Headphones", "tripleS_hatch!_SecretHimitsuBimil"),
+(3, "Tokimetique", "tripleS_hatch!_SecretHimitsuBimil"),
+(4, "TOKYO", "tripleS_hatch!_SecretHimitsuBimil"),
+(5, "Oshare", "tripleS_hatch!_SecretHimitsuBimil"),
+(6, "Untitled", "tripleS_hatch!_SecretHimitsuBimil"),
+(7, "### (hatch! Version)", "tripleS_hatch!_SecretHimitsuBimil"),
+(1, "Password", "tripleS hatch! <Password>"),
+(1, "Pink Power", "Pink Power(잔망루피, tripleS (트리플에스))"),
+(1, "@% (Alpha Percent)", "<ASSEMBLE25>"),
+(2, "깨어 (Are You Alive)", "<ASSEMBLE25>"),
+(3, "추리소설 (Detective Soseol)", "<ASSEMBLE25>"),
+(4, "어제 우리 불꽃놀이 (Firework Diary)", "<ASSEMBLE25>"),
+(5, "Love Child", "<ASSEMBLE25>"),
+(6, "Persona", "<ASSEMBLE25>"),
+(7, "Too Hot", "<ASSEMBLE25>"),
+(8, "Diablo", "<ASSEMBLE25>"),
+(9, "Friend Zone", "<ASSEMBLE25>"),
+(10, "Love2Love", "<ASSEMBLE25>"),
+(1, "Girls Never Die (Japanese Version)", "Girls Never Die (Japanese Version)"),
+(1, "Rising (Japanese Version)", "Rising (Japanese Version)"),
+(4, "PainDrop (by 서연, 지우, 유빈, 소현)", "선의의 경쟁 OST"),
+(1, "PainDrop (by 서연, 지우, 유빈, 소현)", "선의의경쟁 OST Part.4"),
+(2, "PainDrop (by 서연, 지우, 유빈, 소현) (Inst)", "선의의경쟁 OST Part.4"),
+(1, "Girls Never Die (Inst.)", "4study4work4inst Vol.2"),
+(2, "가시권 (Heart Raider) (Inst.)", "4study4work4inst Vol.2"),
+(3, "Midnight Flower (Inst.)", "4study4work4inst Vol.2"),
+(4, "White Soul Sneakers (Inst.)", "4study4work4inst Vol.2"),
+(5, "치유 (Chiyu) (Inst.)", "4study4work4inst Vol.2"),
+(6, "24 (Inst.)", "4study4work4inst Vol.2"),
+(7, "이면의 이면 (Beyond the Beyond) (Inst.)", "4study4work4inst Vol.2"),
+(8, "Non Scale (Inst.)", "4study4work4inst Vol.2"),
+(9, "Dimension (Inst.)", "4study4work4inst Vol.2"),
+(10, "내적 댄스 (Inner Dance) (Inst.)", "4study4work4inst Vol.2"),
+(11, "Hit the Floor (Inst.)", "4study4work4inst Vol.2"),
+(12, "Choom (Inst.)", "4study4work4inst Vol.2"),
+(13, "Éclair (Inst.)", "4study4work4inst Vol.2"),
+(14, "연애소설 (Love Soseol) (Inst.)", "4study4work4inst Vol.2"),
+(15, "Atmosphere (VV Ver.) (Inst.)", "4study4work4inst Vol.2"),
+(16, "12 Rings (Inst.)", "4study4work4inst Vol.2"),
+(17, "Vision (Inst.)", "4study4work4inst Vol.2"),
+(18, "Bionic Power (Inst.)", "4study4work4inst Vol.2"),
+(1, "Dreaming (by 유연, 유빈, 다현, 시온)", "Dreaming (골 때리는 그녀들 X tripleS (트리플에스))"),
+(2, "Dreaming (by 유연, 유빈, 다현, 시온)", "Dreaming (by 유연, 유빈, 다현, 시온) (Inst.)"),
+(1, "Untitled", "tripleS hatch! <Untitled>"),
+(2, "### (hatch! Version)", "tripleS hatch! <Untitled>"),
+(3, "###", "tripleS hatch! <Untitled>"),
+(1, "Untitled", "tripleS hatch! <Untitled>"),
+(1, "Polaroid (by 나경, 마유, 시온, 채원)", "Polaroid"),
+(2, "Polaroid (by 나경, 마유, 시온, 채원) (Inst.)", "Polaroid"),
+(1, "Visual Virtue", "Visionary Vision <Performante>"),
+(2, "Hit the Floor", "Visionary Vision <Performante>"),
+(3, "Choom", "Visionary Vision <Performante>"),
+(4, "Éclair", "Visionary Vision <Performante>"),
+(5, "연애소설 (Love Soseol)", "Visionary Vision <Performante>"),
+(6, "Atmosphere (VV Ver.)", "Visionary Vision <Performante>"),
+(7, "12 Rings", "Visionary Vision <Performante>"),
+(8, "Vision", "Visionary Vision <Performante>"),
+(9, "Bionic Power", "Visionary Vision <Performante>"),
+(10, "Visionary Vision", "Visionary Vision <Performante>"),
+(1, "###", "<###>"),
+(1, "내적 댄스 (Inner Dance)", "Glow <Inner Dance>"),
+(1, "S", "〈ASSEMBLE24〉"),
+(2, "Girls Never Die", "〈ASSEMBLE24〉"),
+(3, "가시권 (Heart Raider)", "〈ASSEMBLE24〉"),
+(4, "Midnight Flower", "〈ASSEMBLE24〉"),
+(5, "White Soul Sneakers", "〈ASSEMBLE24〉"),
+(6, "치유 (Chiyu)", "〈ASSEMBLE24〉"),
+(7, "24", "〈ASSEMBLE24〉"),
+(8, "이면의 이면 (Beyond the Beyond)", "〈ASSEMBLE24〉"),
+(9, "Non Scale", "〈ASSEMBLE24〉"),
+(10, "Dimension", "〈ASSEMBLE24〉");
+
+
+create table all_song_selling
+(
+  so_id int not null auto_increment primary key,
+  title varchar(50),
+  album varchar(50),
+  판매_억단위 int
+);
+
+insert into all_song_selling (title, album, 판매_억단위) values
+("Baby Flower Japanese Version", "Baby Flower Japanese Version", 111),
+("Sad Girls Schemin", "love_and_pop_pt1", 11),
+("Peer", "love_and_pop_pt1", 22),
+("Baby Flower", "love_and_pop_pt1", 95),
+("Type of Girl", "love_and_pop_pt1", 12),
+("Sleek", "love_and_pop_pt1", 66),
+("I Like That", "love_and_pop_pt1", 44),
+("Me Myself Mode", "love_and_pop_pt1", 77),
+("Tokimetique", "Tokimetique", 33),
+("Tokimetique -Shin Sakiura Remix-", "Tokimetique", 22),
+("Tokimetique TV Edit", "Tokimetique", 44),
+("깨어 (Are You Alive) (Inst.)", "4study4work4inst_Vol.3", 44),
+("추리소설 (Detective Soseol) (Inst.)", "4study4work4inst_Vol.3", 33),
+("어제 우리 불꽃놀이 (Firework Diary) (Inst.)", "4study4work4inst_Vol.3", 11),
+("Love Child (Inst.)", "4study4work4inst_Vol.3", 55),
+("Persona (Inst.)", "4study4work4inst_Vol.3", 7),
+("Too Hot (Inst.)", "4study4work4inst_Vol.3", 99),
+("Diablo (Inst.)", "4study4work4inst_Vol.3", 11),
+("Friend Zone (Inst.)", "4study4work4inst_Vol.3", 12),
+("Love2Love (Inst.)", "4study4work4inst_Vol.3", 122),
+("Fly Up (Inst.)", "4study4work4inst_Vol.3", 200),
+("Cameo Love (Inst.)", "4study4work4inst_Vol.3", 55),
+("Bubble Gum Girl (Inst.)", "4study4work4inst_Vol.3", 244),
+("Q&A (Inst.)", "4study4work4inst_Vol.3", 456),
+("Christmas Alone (Inst.)", "4study4work4inst_Vol.3", 600),
+("Magic Shine New Zone", "msnz_Beyond_Beauty", 66),
+("Fly Up", "msnz_Beyond_Beauty", 255),
+("Cameo Love", "msnz_Beyond_Beauty", 1225),
+("Bubble Gum Girl", "msnz_Beyond_Beauty", 1200),
+("Q&A", "msnz_Beyond_Beauty", 4000),
+("Christmas Alone", "msnz_Beyond_Beauty", 3900),
+("Password", "tripleS_hatch!_SecretHimitsuBimil", 1244),
+("Headphones", "tripleS_hatch!_SecretHimitsuBimil", 256),
+("Tokimetique", "tripleS_hatch!_SecretHimitsuBimil", 44),
+("TOKYO", "tripleS_hatch!_SecretHimitsuBimil", 55),
+("Oshare", "tripleS_hatch!_SecretHimitsuBimil", 755),
+("Untitled", "tripleS_hatch!_SecretHimitsuBimil", 245),
+("### (hatch! Version)", "tripleS_hatch!_SecretHimitsuBimil", 555),
+("Password", "tripleS hatch! <Password>", 755),
+("Pink Power", "Pink Power(잔망루피, tripleS (트리플에스))", 14),
+("@% (Alpha Percent)", "<ASSEMBLE25>", 55),
+("깨어 (Are You Alive)", "<ASSEMBLE25>", 2244),
+("추리소설 (Detective Soseol)", "<ASSEMBLE25>", 1255),
+("어제 우리 불꽃놀이 (Firework Diary)", "<ASSEMBLE25>", 456),
+("Love Child", "<ASSEMBLE25>", 665),
+("Persona", "<ASSEMBLE25>", 476),
+("Too Hot", "<ASSEMBLE25>", 1100),
+("Diablo", "<ASSEMBLE25>", 245),
+("Friend Zone", "<ASSEMBLE25>", 665),
+("Love2Love", "<ASSEMBLE25>", 600),
+("Girls Never Die (Japanese Version)", "Girls Never Die (Japanese Version)", 1222),
+("Rising (Japanese Version)", "Rising (Japanese Version)", 5554),
+("Dreaming (by 유연, 유빈, 다현, 시온)", "Dreaming (골 때리는 그녀들 X tripleS (트리플에스))", 222),
+("Dreaming (by 유연, 유빈, 다현, 시온)", "Dreaming (by 유연, 유빈, 다현, 시온) (Inst.)", 45);
+
+select title
+from all_song_selling
+where 판매_억단위 < all (
+select 판매_억단위
+from all_song_selling
+where title in ("Dreaming (by 유연, 유빈, 다현, 시온)", "Baby Flower Japanese Version")
+);
+
+
+/*
+출력값
++-----------------------------------------------------+
+| title                                               |
++-----------------------------------------------------+
+| Sad Girls Schemin                                   |
+| Peer                                                |
+| Type of Girl                                        |
+| I Like That                                         |
+| Tokimetique                                         |
+| Tokimetique -Shin Sakiura Remix-                    |
+| Tokimetique TV Edit                                 |
+| 깨어 (Are You Alive) (Inst.)                        |
+| 추리소설 (Detective Soseol) (Inst.)                 |
+| 어제 우리 불꽃놀이 (Firework Diary) (Inst.)         |
+| Persona (Inst.)                                     |
+| Diablo (Inst.)                                      |
+| Friend Zone (Inst.)                                 |
+| Tokimetique                                         |
+| Pink Power                                          |
++-----------------------------------------------------+
+*/
