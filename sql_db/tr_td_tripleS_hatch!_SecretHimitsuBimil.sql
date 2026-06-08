f1035a549187f26650d5a53f22981209ebb16d96
groomigroom <googjohn0143@gmail.com>
2026-06-09 06:36:52 +0900

    Update and rename edit__tripleS_hatch!_SecretHimitsuBimil.sql to tr_td_tripleS_hatch!_SecretHimitsuBimil.sql

---
+++ sql_db/tr_td_tripleS_hatch!_SecretHimitsuBimil.sql
@@ -0,0 +1,18 @@
+create table tripleS_hatch!_SecretHimitsuBimil
+(
+  title varchar(50),
+  track_num integer,
+  song_len time,
+  melon_like integer,
+  youtube_like integer
+);
+
+insert into tripleS_hatch!_SecretHimitsuBimil
+values
+("Password", 1, "00:00:00", 400, 36000),
+("Headphones", 2, "00:00:00", 255, 2500),
+("Tokimetique", 3, "00:00:00", 261, 2100),
+("TOKYO", 4, "00:00:00", 274, 1800),
+("Oshare", 5, "00:00:00", 346, 3900),
+("Untitled", 6, "00:00:00", 190, 28000),
+("### (hatch! Version)", 7, "00:00:00", 201, 1000);
