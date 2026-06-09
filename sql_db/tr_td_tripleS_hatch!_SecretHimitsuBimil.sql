7c4e9de8be28865b3799858f932d305697c3a5e4
groomigroom <googjohn0143@gmail.com>
2026-06-09 13:54:32 +0900

    Update tr_td_tripleS_hatch!_SecretHimitsuBimil.sql

--- sql_db/tr_td_tripleS_hatch!_SecretHimitsuBimil.sql
+++ sql_db/tr_td_tripleS_hatch!_SecretHimitsuBimil.sql
@@ -1,4 +1,4 @@
-create table tripleS_hatch느낌표_SecretHimitsuBimil
+create table tripleS_hatch!_SecretHimitsuBimil
 (
   title varchar(50),
   track_num integer,
@@ -7,12 +7,12 @@ create table tripleS_hatch느낌표_SecretHimitsuBimil
   youtube_like integer
 );
 
-insert into tripleS_hatch느낌표_SecretHimitsuBimil
+insert into tripleS_hatch!_SecretHimitsuBimil
 values
-("Password", 1, "00:00:00", 400, 36000),
-("Headphones", 2, "00:00:00", 255, 2500),
-("Tokimetique", 3, "00:00:00", 261, 2100),
-("TOKYO", 4, "00:00:00", 274, 1800),
-("Oshare", 5, "00:00:00", 346, 3900),
-("Untitled", 6, "00:00:00", 190, 28000),
-("### (hatch! Version)", 7, "00:00:00", 201, 1000);
+("Password", 1, "00:02:55", 400, 36000),
+("Headphones", 2, "00:03:00", 255, 2500),
+("Tokimetique", 3, "00:02:45", 261, 2100),
+("TOKYO", 4, "00:02:29", 274, 1800),
+("Oshare", 5, "00:03:24", 346, 3900),
+("Untitled", 6, "00:02:41", 190, 28000),
+("### (hatch! Version)", 7, "00:03:10", 201, 1000);
